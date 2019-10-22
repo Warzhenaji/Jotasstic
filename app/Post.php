ffbc2de9e027d6912e54811bc3fba0994d04926c
natewdh <nate@discoverhealing.com>
2019-10-22 11:03:45 -0600

    Removed Commenting Functionality

--- app/Post.php
+++ app/Post.php
@@ -18,8 +18,4 @@ class Post extends Model
     public function user() {
     	return $this->belongsTo(User::class);
     }
-
-    public function comments() {
-        return $this->morphMany(Comment::class, 'commentable');
-    }
 }
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -11,9 +11,6 @@
 			<div class="rounded-b px-6 py-4 shadow-lg text-gray-400 bg-nim-blue opacity-100">
 				<p class="text-sm">{{ $post->description }}</p>
 			</div>
-			<div class="px-6 py-4 rounded-b rounded-t card-bg shadow-lg mt-6">
-				<p class="text-center">Meta Data and Commenting Coming Soon!</p>
-			</div>
 		</div>
 	</div>
 @endsection
