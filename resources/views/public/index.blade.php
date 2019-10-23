ae5acd207f15293e523e3f2b9278492e1eaabd8d
natewdh <nate@discoverhealing.com>
2019-10-23 15:57:39 -0600

    Home Page Cascade Alignment
    Changed home page cascade layout to be correctly displayed. Added a few tasks to the tasks list.

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-	@foreach($posts as $post)
-		{{ $post->title }}
-		<img src="/img/uploads/{{ $post->media }}">
+	@foreach($posts->chunk(5) as $postChunk)
+		<div class="flex flex-col items-center mt-4">
+			<div class="flex justify-between w-4/5 post-card-row">
+				@foreach($postChunk as $post)
+					@include('dashboard.partials.post_card', ['post' => $post])
+				@endforeach
+			</div>
+		</div>
 	@endforeach
 @endsection
