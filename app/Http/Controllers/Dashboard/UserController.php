60629b9d4e6f44585bd39d60069fdbeaf79989bb
natewdh <nate@discoverhealing.com>
2019-11-21 12:21:23 -0700

    Change Password Complete
    Auth'd user can now change password securely and without error.

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Dashboard;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -24,12 +25,16 @@ class UserController extends Controller
 
     public function updateUser(Request $request) {
         $user = auth()->user();
+
         $user->name = $request->input('name');
         $user->email = $request->input('email');
-        $user->password = $request->input('psw');
+        $password = $request->input('psw');
         $password_confirm = $request->input('psw_confirm');
-        if ($password_confirm === $user->password || $password_confirm == null || $user->password == null) {
-            if ($user->update()) {
+
+        if ($password != null && $password_confirm != null && $password == $password_confirm) {
+           $user->password = Hash::make($password);
+        }
+         if ($user->update()) {
                 session()->flash('status', [
                     'error' => false,
                     'title' => "Success",
@@ -43,13 +48,5 @@ class UserController extends Controller
                 'message' => "Something went wrong..."
             ]);
             return redirect()->back();
-        } else {
-            session()->flash('status', [
-                'error' => true,
-                'title' => "Aw snap, Gur.",
-                'message' => "Something went wrong..."
-            ]);
-            return redirect()->back();
-        }
     }
 }
