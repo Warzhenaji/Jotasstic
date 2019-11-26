6220760e7a9077bfab3678b19f3f1e78ae74b156
natewdh <nate@discoverhealing.com>
2019-11-26 16:02:59 -0700

    Attempted Migration Refactor
    Not sure if this worked.

--- database/migrations/2019_07_05_204554_create_art_table.php
+++ database/migrations/2019_07_05_204554_create_art_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArtTable extends Migration
+class CreateArtsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -34,6 +34,6 @@ class CreateArtTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('art');
+        Schema::dropIfExists('arts');
     }
 }
