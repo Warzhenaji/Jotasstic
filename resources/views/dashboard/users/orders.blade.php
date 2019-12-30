b13ea3572bbf455c786b22f60c69512f95d95ea1
natewdh <nate@discoverhealing.com>
2019-12-30 15:59:02 -0700

    Orders Page Pt. 1
    Skeleton of orders report list.

--- resources/views/dashboard/users/orders.blade.php
+++ resources/views/dashboard/users/orders.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>Orders</h1>
+	<div class="flex bg-contain orders-background w-full h-full">
+		<div class="flex w-full rounded overflow-hidden shadow-lg bg-white ml-40 mr-40 mt-20 mb-20">
+			<table class="table-auto">
+				<thead class="flex content-between">
+					<tr class="justify-start">
+						<th class="w-1/2 px-4 py-2">Name</th>
+						<th class="w-1/4 px-4 py-2">Item Ordered</th>
+						<th class="w-1/4 py-2">Price</th>
+						<th class="w-1/4 py-2">Payment Received</th>
+						<th class="w-1/4 py-2">Shipped</th>
+						<th class="w-1/4 py-2">Order Complete</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class="border px-4 py-2">Bill Jenkins</td>
+					    <td class="border px-4 py-2">Saudade</td>
+					    <td class="border px-4 py-2">25</td>
+					    <td class="border px-4 py-2">Yes</td>
+					    <td class="border px-4 py-2">Yes</td>
+					    <td class="border px-4 py-2">Yes</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 @endsection
